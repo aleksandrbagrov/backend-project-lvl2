6a62ed3c7cb2867ed120f9f65e3c78d36da2f218
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-07-26 15:07:56 +0300

    modify index

--- src/index.js
+++ src/index.js
@@ -14,33 +14,48 @@ const genDiff = (filepath1, filepath2) => {
   const obj1 = parsers(getDoc(filepath1), getFileExtension(filepath1));
   const obj2 = parsers(getDoc(filepath2), getFileExtension(filepath2));
 
-  const keysObj1 = Object.keys(obj1);
-  const keysObj2 = Object.keys(obj2);
-  const diff1 = _.difference(keysObj1, keysObj2);
-  const intersection = _.intersection(keysObj1, keysObj2);
-  const diff2 = _.difference(keysObj2, keysObj1);
-  const resArr = _.sortBy([...diff1, ...intersection, ...diff2]);
-  const resObj = resArr.reduce((acc, item) => {
-    if (diff1.includes(item)) {
-      acc[`- ${item}`] = obj1[item];
-      return acc;
-    }
-    if (diff2.includes(item)) {
-      acc[`+ ${item}`] = obj2[item];
-      return acc;
-    }
-    if (intersection.includes(item)) {
-      if (obj1[item] === obj2[item]) {
-        acc[`  ${item}`] = obj1[item];
-        return acc;
-      } else {
+  const iter = (val1, val2) => {
+    const keysObj1 = Object.keys(obj1);
+    console.log(keysObj1);
+    const keysObj2 = Object.keys(obj2);
+    console.log(keysObj2);
+    const diff1 = _.difference(keysObj1, keysObj2);
+    console.log(diff1);
+    const intersection = _.intersection(keysObj1, keysObj2);
+    console.log(intersection);
+    const diff2 = _.difference(keysObj2, keysObj1);
+    console.log(diff2);
+    const resArr = _.sortBy([...diff1, ...intersection, ...diff2]); //, ([k, v]) => [k]);
+    console.log(resArr);
+
+    const resObj = resArr.reduce((acc, item) => {
+      if (diff1.includes(item)) {
+        console.log(`Item: ${item} with value: ${obj1[item]}`);
         acc[`- ${item}`] = obj1[item];
+        return acc;
+      }
+      if (diff2.includes(item)) {
         acc[`+ ${item}`] = obj2[item];
         return acc;
       }
-    }
-  }, {});
-  const resStr = JSON.stringify(resObj, null, '  ').replace(/[",]/g, '');
+      if (intersection.includes(item)) {
+        if (!_.isObject(obj1[item]) && !_.isObject(obj1[item])) {
+          if (obj1[item] === obj2[item]) {
+            acc[`  ${item}`] = obj1[item];
+            return acc;
+          } else {
+            acc[`- ${item}`] = obj1[item];
+            acc[`+ ${item}`] = obj2[item];
+            return acc;
+          }
+        }
+      }
+    }, {});
+    return resObj;
+  };
+  //iter(obj1, obj2);
+
+  const resStr = JSON.stringify(iter(obj1, obj2), null, '  ').replace(/[",]/g, '');
 
   return resStr;
 };
