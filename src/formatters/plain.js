1b1386406fc69c8568611604f44b54de5097d302
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-08-25 22:02:47 +0300

    modify plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 const getFormattedValue = (value) => {
+  _.isObject(value) ? '[complex value]' : value;
   if (typeof value === 'string' && value !== '[complex value]') {
     return `\x1b[0;31m'${value}'\x1b[0m`;
   } else if (typeof value === 'boolean') {
@@ -12,13 +13,20 @@ const getFormattedValue = (value) => {
 
 const plain = (object) => {
   const iter = (currObj, keyPath) => {
-    //if (typeof currObj !== 'object' || currObj === null) {
-    //  return `${currObj}`;
-    //}
-    let pathKey = keyPath;
-    let prevPathKey = pathKey;
+    const pathKey = keyPath;
     const entries = Object.entries(currObj);
-    const resEntries = [];
+    const resEntries = entries.reduce((acc, item, i) => {
+      if (i + 1 < entries.length && item[0].slice(2) === entries[i + 1][0].slice(2)) {
+        acc.push(entries.slice(i, i + 2));
+        return acc;
+      } else {
+        acc.push(entries[i]);
+        return acc;
+      }
+    }, []);
+    console.log(`Res Entries : `);
+    console.log(resEntries);
+    /*const resEntries = [];
     let i = 0;
     do {
       if (i + 1 < entries.length && entries[i][0].slice(2) === entries[i + 1][0].slice(2)) {
@@ -28,40 +36,39 @@ const plain = (object) => {
         resEntries.push(entries[i]);
         i += 1;
       }
-    } while (i < entries.length);
+    } while (i < entries.length);*/
 
     const line = resEntries.reduce((acc, [key, value]) => {
       const currentKey = Array.isArray(key) ? key[0].slice(2) : key.slice(2);
-      pathKey += pathKey !== '' ? '.' + currentKey : currentKey;
-      if (!Array.isArray(key) && key.startsWith(' ')) {
-        _.isObject(value) ? acc.push(iter(value, pathKey)) : '';
-        pathKey = prevPathKey;
-        return acc;
-      }
-      if (!Array.isArray(key) && key.startsWith('-')) {
-        acc.push(`Property \x1b[0;31m'${pathKey}'\x1b[0m was removed`);
-        pathKey = prevPathKey;
-        return acc;
-      }
-      if (!Array.isArray(key) && key.startsWith('+')) {
-        const valueAdd = _.isObject(value) ? '[complex value]' : value;
-        acc.push(
-          `Property \x1b[0;31m'${pathKey}'\x1b[0m was added with value: ${getFormattedValue(
-            valueAdd
-          )}`
-        );
-        pathKey = prevPathKey;
-        return acc;
+      pathKey.push(currentKey);
+      if (!Array.isArray(key)) {
+        if (key.startsWith(' ')) {
+          _.isObject(value) ? acc.push(iter(value, pathKey)) : '';
+          pathKey.pop();
+          return acc;
+        }
+        if (key.startsWith('-')) {
+          acc.push(`Property \x1b[0;31m'${pathKey.join('.')}'\x1b[0m was removed`);
+          pathKey.pop();
+          return acc;
+        }
+        if (key.startsWith('+')) {
+          acc.push(
+            `Property \x1b[0;31m'${pathKey.join(
+              '.'
+            )}'\x1b[0m was added with value: ${getFormattedValue(value)}`
+          );
+          pathKey.pop();
+          return acc;
+        }
       }
       if (Array.isArray(key)) {
-        const valueFrom = _.isObject(key[1]) ? '[complex value]' : key[1];
-        const valueTo = _.isObject(value[1]) ? '[complex value]' : value[1];
         acc.push(
-          `Property \x1b[0;31m'${pathKey}'\x1b[0m was updated. From ${getFormattedValue(
-            valueFrom
-          )} to ${getFormattedValue(valueTo)}`
+          `Property \x1b[0;31m'${pathKey.join('.')}'\x1b[0m was updated. From ${getFormattedValue(
+            key[1]
+          )} to ${getFormattedValue(value[1])}`
         );
-        pathKey = prevPathKey;
+        pathKey.pop();
         return acc;
       }
     }, []);
@@ -69,7 +76,7 @@ const plain = (object) => {
     return line.join('\n');
   };
 
-  return iter(object, '');
+  return iter(object, []);
 };
 
 export default plain;
